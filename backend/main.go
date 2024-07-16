4f061ff56c35bfa76ebb5f80192469324861623f
DenisKantic <denis.kantic18@gmail.com>
2024-07-16 22:15:59 +0200

    added golang code for backend logic and modified gitignore to ignore file server files

--- backend/main.go
+++ backend/main.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 type Response struct {
@@ -24,6 +25,7 @@ type File struct {
 	Filename   string `json:"filename"`
 	Filetype   string `json:"filetype"`
 	Filesize   int64  `json:"filesize"`
+	Filepath   string `json:"filepath"`
 	UploadedAt string `json:"uploaded_at"`
 }
 
@@ -47,39 +49,36 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 
 	err := r.ParseMultipartForm(10 << 20) // 10 MB max
 	if err != nil {
-		fmt.Println("Error happened in processing form", err)
+		http.Error(w, "Error processing form", http.StatusInternalServerError)
 		return
 	}
 
 	file, handler, err := r.FormFile("file")
 	if err != nil {
-		fmt.Println("Error happened in getting form file", err)
+		http.Error(w, "Error getting form file", http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
 
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
-
-	fileBytes, err := io.ReadAll(file)
-	if err != nil {
-		fmt.Println("Error reading file", err)
-		return
-	}
-
+	// Create a file on the server
 	savedFilePath := fmt.Sprintf("db_files/%s", handler.Filename)
-	err = os.WriteFile(savedFilePath, fileBytes, 0644)
+	dst, err := os.Create(savedFilePath)
 	if err != nil {
-		fmt.Println("Error saving file to disk", err)
+		http.Error(w, "Error saving file to disk", http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	// Copy the uploaded file to the server file
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, "Error saving file to disk", http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Printf("Successfully uploaded file. Saved path: %s\n", savedFilePath)
-
-	err = saveFilePathToDB(handler.Filename, handler.Header.Get("Content-Type"), handler.Size)
+	// Save the file path in the database
+	err = saveFilePathToDB(handler.Filename, handler.Header.Get("Content-Type"), handler.Size, savedFilePath)
 	if err != nil {
-		fmt.Println("Error saving file to database", err)
+		http.Error(w, "Error saving file to database", http.StatusInternalServerError)
 		return
 	}
 
@@ -91,7 +90,7 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-func saveFilePathToDB(filename, filetype string, filesize int64) error {
+func saveFilePathToDB(filename, filetype string, filesize int64, filepath string) error {
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
 		DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT)
 
@@ -101,8 +100,8 @@ func saveFilePathToDB(filename, filetype string, filesize int64) error {
 	}
 	defer db.Close()
 
-	query := `INSERT INTO dbFiles (filename, filetype, filesize) VALUES ($1, $2, $3)`
-	_, err = db.Exec(query, filename, filetype, filesize)
+	query := `INSERT INTO dbFiles (filename, filetype, filesize, filepath, uploaded_at) VALUES ($1, $2, $3, $4, $5)`
+	_, err = db.Exec(query, filename, filetype, filesize, filepath, time.Now())
 	if err != nil {
 		return fmt.Errorf("Error saving file to database: %v", err)
 	}
@@ -125,7 +124,7 @@ func getFiles(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT id, filename, filetype, filesize, uploaded_at FROM dbFiles")
+	rows, err := db.Query("SELECT id, filename, filetype, filesize, filepath, uploaded_at FROM dbFiles")
 	if err != nil {
 		http.Error(w, "Error retrieving files from database", http.StatusInternalServerError)
 		return
@@ -136,7 +135,7 @@ func getFiles(w http.ResponseWriter, r *http.Request) {
 
 	for rows.Next() {
 		var file File
-		err := rows.Scan(&file.ID, &file.Filename, &file.Filetype, &file.Filesize, &file.UploadedAt)
+		err := rows.Scan(&file.ID, &file.Filename, &file.Filetype, &file.Filesize, &file.Filepath, &file.UploadedAt)
 		if err != nil {
 			http.Error(w, "Error scanning file data", http.StatusInternalServerError)
 			return
@@ -167,7 +166,7 @@ func deleteFiles(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	id, err := strconv.Atoi(idString) // need to google what is this
+	id, err := strconv.Atoi(idString)
 	if err != nil {
 		http.Error(w, "Invalid file ID", http.StatusBadRequest)
 		return
@@ -181,7 +180,6 @@ func deleteFiles(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error connecting to database", http.StatusInternalServerError)
 		return
 	}
-
 	defer db.Close()
 
 	query := `DELETE FROM dbFiles WHERE id = $1`
@@ -197,20 +195,77 @@ func deleteFiles(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(response)
 }
-func setupRoutes() {
 
+// download file Function
+func downloadFile(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Disposition")
+
+	if r.Method == http.MethodOptions {
+		return
+	}
+
+	idString := r.URL.Query().Get("id")
+	if idString == "" {
+		http.Error(w, "Missing id parameter", http.StatusBadRequest)
+		return
+	}
+
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		http.Error(w, "invalid file ID", http.StatusBadRequest)
+		return
+	}
+
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT)
+
+	db, err := sql.Open("postgres", dbinfo)
+	if err != nil {
+		http.Error(w, "Error connecting to database", http.StatusInternalServerError)
+		return
+	}
+
+	defer db.Close()
+
+	var filename, filepath string
+	query := `SELECT filename, filepath FROM dbFiles WHERE id = $1`
+	err = db.QueryRow(query, id).Scan(&filename, &filepath)
+	if err != nil {
+		http.Error(w, "Error retrieving file from database", http.StatusInternalServerError)
+		log.Printf("Error retrieving file from database: %v", err)
+		return
+	}
+	// Get base filename from the full filename path
+	//baseFilename := path.Base(filename)
+
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%q", filename))
+	w.Header().Set("Content-Type", "application/octet-stream")
+
+	message := fmt.Sprintf(filename)
+
+	response := Response{
+		Message: message,
+	}
+
+	json.NewEncoder(w).Encode(response)
+	http.ServeFile(w, r, filepath)
+}
+
+func setupRoutes() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/upload", uploadFile)
 	mux.HandleFunc("/files", getFiles)
 	mux.HandleFunc("/delete", deleteFiles)
+	mux.HandleFunc("/download", downloadFile)
 	corsHandler := cors.Default().Handler(mux)
 
 	log.Fatal(http.ListenAndServe(":8080", corsHandler))
 }
 
 func main() {
-
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file")
